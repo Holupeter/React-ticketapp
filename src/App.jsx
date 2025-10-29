b9c4f628bc22c8f3b567287eb135f8e5c48e1299
Orion <holupeter2@gmail.com>
2025-10-29 18:52:58 +0100

    HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Routes, Route } from 'react-router-dom';
 
 import './styles.css';
 import Navbar from './components/Navbar';
