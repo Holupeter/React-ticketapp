640768e821d0dbbad787e3db9d68393af3086617
Orion <holupeter2@gmail.com>
2025-10-29 20:12:29 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BBrowserRouter as  Routes, Route } from 'react-router-dom';
 
 import './styles.css';
 import Navbar from './components/Navbar';
